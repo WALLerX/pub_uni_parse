0e0588dbb6a6764245837f3cf2f132586587ed99
WALLer <waller@bk.ru>
2022-12-06 16:00:50 +0700

    scheduler

---
+++ scheduler/cron.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "SHELL=/bin/bash" > /etc/cron.d/reguest-cron
+echo "${CRON_AVITO} root curl -X GET -H \"Content-Type: application/json\" \"http://parser_avito:8080/api?action=start_parse\" >> /var/log/cron.log 2>&1" >> /etc/cron.d/reguest-cron
+echo "${CRON_CIAN} root curl -X GET -H \"Content-Type: application/json\" \"http://parser_cian:8080/api?action=start_parse\" >> /var/log/cron.log 2>&1" >> /etc/cron.d/reguest-cron
+echo "${CRON_SIBDOM} root curl -X GET -H \"Content-Type: application/json\" \"http://parser_sibdom:8080/api?action=start_parse\" >> /var/log/cron.log 2>&1" >> /etc/cron.d/reguest-cron
+echo "${CRON_DOMCLICK} root curl -X GET -H \"Content-Type: application/json\" \"http://parser_domclick:8080/api?action=start_parse\" >> /var/log/cron.log 2>&1" >> /etc/cron.d/reguest-cron
+
+crontab /etc/cron.d/reguest-cron
+chmod 0644 /etc/cron.d/reguest-cron
+touch /var/log/cron.log
+
+/etc/init.d/cron start
+tail -f /dev/null
