d54169e217402a6f2e48ebffe4499186955b606d
WALLer <waller@bk.ru>
2022-12-27 21:07:27 +0700

    11

--- scheduler/cron.sh
+++ scheduler/cron.sh
@@ -4,7 +4,8 @@ echo "${CRON_AVITO} root curl -X GET -H \"Content-Type: application/json\" \"htt
 echo "${CRON_CIAN} root curl -X GET -H \"Content-Type: application/json\" \"http://parser_cian:8080/api?action=start_parse\" >> /var/log/cron.log 2>&1" >> /etc/cron.d/reguest-cron
 echo "${CRON_SIBDOM} root curl -X GET -H \"Content-Type: application/json\" \"http://parser_sibdom:8080/api?action=start_parse\" >> /var/log/cron.log 2>&1" >> /etc/cron.d/reguest-cron
 echo "${CRON_DOMCLICK} root curl -X GET -H \"Content-Type: application/json\" \"http://parser_domclick:8080/api?action=start_parse\" >> /var/log/cron.log 2>&1" >> /etc/cron.d/reguest-cron
-echo "";
+echo "
+";
 
 crontab /etc/cron.d/reguest-cron
 chmod 0644 /etc/cron.d/reguest-cron
